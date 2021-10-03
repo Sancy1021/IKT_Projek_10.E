0badfa5e0b6132fb75cd9b0e956e4756ddee5c4a
Sancy21 <90603335+Sancy1021@users.noreply.github.com>
2021-10-03 16:20:46 +0200

    Add files via upload

---
+++ Hazi.py
@@ -0,0 +1,5 @@
+adat1 = input(' szoveg ')
+adat2 = int(input(' szam '))
+adat3 = input(' szoveg2 ')
+adat4 = float(input(' tizedes '))
+print ('  ide jön a  '  +  adat1 +  ' ide jön a  '  ,adat2, ' ide jön a  '  +  adat3  +  ' ide jön a  '  ,adat4,)

23bd3dd28b3ef78f888fa33dd64da76e2514db3b
Sancy21 <90603335+Sancy1021@users.noreply.github.com>
2021-10-04 16:00:44 +0200

    Delete Hazi.py

--- Hazi.py
+++
@@ -1,5 +0,0 @@
-adat1 = input(' szoveg ')
-adat2 = int(input(' szam '))
-adat3 = input(' szoveg2 ')
-adat4 = float(input(' tizedes '))
-print ('  ide jön a  '  +  adat1 +  ' ide jön a  '  ,adat2, ' ide jön a  '  +  adat3  +  ' ide jön a  '  ,adat4,)

573470286a3aea3e00103bccba467ad0d3660e64
Sancy21 <90603335+Sancy1021@users.noreply.github.com>
2021-10-04 18:03:15 +0200

    Add files via upload

---
+++ Magaság.py
@@ -0,0 +1,11 @@
+print('170 és 180 közöt átlagos vagy ')
+cm1 = int(input(' cm1 '))
+
+print('170 allat törpe vagy')
+cm2 = int(input(' cm2 '))
+
+print('180 felet zsirád vagy')
+cm3 = int(input(' cm3 '))
+
+print('170 és 180 közöt átlagosvagy'  ,cm1,  'ha 170 alatt vagy akkor törpe vagy' ,cm2, 'ha 180 felet vagy akkor zsiráf vagy'  ,cm3, )
+

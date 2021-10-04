49a766725dba62c2db584c13da05d3babd4f6b52
Sancy21 <90603335+Sancy1021@users.noreply.github.com>
2021-10-04 10:35:28 +0200

    Add files via upload

--- orai munka.py
+++ orai munka.py
@@ -1,17 +1,37 @@
 print('1.feladat')
 print('Szia '  + input('Neved?') + '!')
+
 print('2.feladat')
 print('**********')
 print('oooooooooo')
+
 print('3.feleadat')
 print('*o*o*o*o*o')
 print('*o*o*o*o*o')
+
 print('4.feladat')
 szam = int(input(' szam '))
 szam2 = int(input(' szam2 '))
 print ('szamold ki ezt ' ,szam+szam2)
+
 print('5.feledat')
 szam3 = int(input(' szam3 '))
 szam4 = int(input(' szam4 '))
 szam5 = int(input(' szam5 '))
 print ('szamold ki ezeket is  ' ,szam3+szam4+szam5)
+
+print('6.feladat')
+szam6 = int(input(' szam6 '))
+szam7 = int(input(' szam7 '))
+print('A két szám hányadosa: ',szam6-szam7)
+
+print('7.feladat')
+szam8 = int(input(' szam8 '))
+szam9 = int(input(' szam9 '))
+print('A két szám szorzata: ',szam8*szam9)
+
+print('8.feladat')
+szam10 = int(input(' szam10 '))
+szam11 = int(input(' szam11 '))
+szam12 = float( szam10/szam11 )
+print('A két szám szorzata: ',szam12)

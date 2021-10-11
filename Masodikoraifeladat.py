9572ae630d26a16751c0ae1bce21b650b55e90b0
Sancy21 <90603335+Sancy1021@users.noreply.github.com>
2021-10-11 10:30:54 +0200

    Add files via upload

---
+++ Masodikoraifeladat.py
@@ -0,0 +1,27 @@
+print(' 1. feladat ')
+
+x = int(input(' Adj meg egy számot! '))
+
+z = int(input(' Adj meg egy másik számot! '))
+
+y = int(input(' Adja meg harmadikszámot! '))
+
+a=min(x,z,y)
+print('A legkisebb szám',a,)
+
+
+print(' 2. feladat ')
+o = int(input(' Adj meg egy számot! '))
+
+e = int(input(' Adj meg egy másik számot! '))
+
+g = int(input(' Adja meg harmadikszámot! '))
+
+b=max(o,e,g)
+print('A legnyagobb szám',b,)
+
+print(' 3. feladat ')
+
+
+
+

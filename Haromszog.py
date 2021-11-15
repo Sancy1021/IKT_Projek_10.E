89cc0531692efdf3ffa58fb17c7028e0fa6cc0e0
Sancy21 <90603335+Sancy1021@users.noreply.github.com>
2021-11-15 10:34:17 +0100

    Add files via upload

---
+++ Haromszog.py
@@ -0,0 +1,12 @@
+import time
+import os 
+def CLS():
+        os.system('CLS')
+CLS()
+
+for x in reversed (range (8)):
+    print (x*"0")
+    time.sleep(1/10)
+for x in  range (8):
+    print (x*"0")
+    time.sleep(1/10)

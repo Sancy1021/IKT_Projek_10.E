00f222cad22f3a6787444fa2a18c85311a5c710f
Sancy21 <90603335+Sancy1021@users.noreply.github.com>
2021-11-22 10:30:47 +0100

    Add files via upload

---
+++ Feladat 12 töl.py
@@ -0,0 +1,45 @@
+print ('12dik feladat')
+
+for i in  range (0,11,2):
+    print(i)
+
+print('13dik feladat')
+
+for i in range (0,101,3):
+    print(i)
+
+print('14dik feladat')
+szam = int(input( ' a szamot szorozd meg 2 vel ' ))
+for i in range (szam*2):
+    print(i)
+
+print('15dik feladat')
+
+for i in range (0,6):
+    print(i)
+
+print('16dik feladat')
+
+for i in range (1,11):
+    print(i*i)
+
+print('17dik feladat') 
+
+for i in range (1,32):
+    print(i*i)
+
+print('18dik feladat')
+
+for i in range (0,101,2):
+    print(i)
+
+print('19dik feladat')
+szam2 = int(input( ' adj meg egy számot ' ))
+szam3 = int(input( ' adj meg egy még egy számot ' ))
+print(szam3*szam3,szam2*szam2)
+
+print('20dik feladat')
+
+for i in range (51,9-2):
+    print(i)
+

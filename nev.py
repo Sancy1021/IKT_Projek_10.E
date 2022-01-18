18995b95c5ac554f58780be38b8e2b5f6dadbd52
Sancy21 <90603335+Sancy1021@users.noreply.github.com>
2022-01-18 14:23:02 +0100

    Add files via upload

---
+++ nev.py
@@ -0,0 +1,23 @@
+from importlib.machinery import all_suffixes
+
+
+while True:
+        
+
+        name=input(' Add meg a nevet ')
+            
+
+        if name == 'Zsidó':
+            print('elso nev a nev jo')
+            break 
+        elif name == 'Stalin':
+            print('masodik nev a nev jo')
+            break
+        elif name == 'Hitler':
+            print('harmadik nev a nev jo')
+            break
+        else:
+            print('NEM jo a nev')
+            break
+
+   

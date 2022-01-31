9f9dce74528ccdd691bef7682567435ea81147c7
Sancy21 <90603335+Sancy1021@users.noreply.github.com>
2022-01-31 12:00:44 +0100

    Add files via upload

--- Korsugara.py
+++ Korsugara.py
@@ -5,8 +5,8 @@ def kerulet (r):
 def terulet (r):
     pi=3.14
     return(r**2)*(pi)
-sugar=float(input("A kör sugara"))
+sugar=float(input("A kör sugara "))
 K=kerulet(sugar)
 T=terulet(sugar)
-print(" A körterülete: [0] egység")
-print(' A Kerület: [0] négzetegység.format'(T))
+print(" A körterülete: [0] egység".format(K))
+print(' A Kerület: [0] négzetegység.'.format(T))
